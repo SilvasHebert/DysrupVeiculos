5a07142467e03e119ea7de8bf77724a76f5c8151
hebert.silva@cibus.app.br <hebert.silva@cibus.app.br>
2024-03-18 23:39:16 -0300

    correção case sensitive

--- ios/DysrupVeiculosTests/DysrupVeiculosTests.m
+++ ios/DysrupVeiculosTests/DysrupVeiculosTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface DysrupVeiculosTests : XCTestCase
+@interface dysrupveiculosTests : XCTestCase
 
 @end
 
-@implementation DysrupVeiculosTests
+@implementation dysrupveiculosTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
